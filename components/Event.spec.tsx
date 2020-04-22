c30b69e35170e8520cb0a9aa5718bc963197143d
Lucas Gladding <l.gladding@vehikl.com>
2020-04-21 22:11:55 -0400

    Added test for event component.

---
+++ components/Event.spec.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {render} from '@testing-library/react-native';
+
+import Event from './Event';
+
+describe('Event', () => {
+  const data = {
+    name: 'Event',
+    description: 'Lorem ipsum...',
+    starts_at: 'April 22, 2020 @ 7:00 PM',
+    attendees_count: '50 attendees',
+  };
+
+  it('renders', () => {
+    const {getByText} = render(<Event name={data.name} description={data.description} starts_at={data.starts_at} attendees_count={data.attendees_count} />);
+    expect(getByText(data.name)).toBeDefined();
+    expect(getByText(sanitize(data.description))).toBeDefined();
+    expect(getByText(data.starts_at)).toBeDefined();
+    expect(getByText(data.attendees_count)).toBeDefined();
+  });
+});
+
+const sanitize = (input: string) => {
+  return (content: string) => {
+    return input.replace(/\n+/g, ' ') == content;
+  };
+};
---
+++ components/Event.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {Text, View} from "react-native";
+
+interface EventProps {
+  name: string;
+  description: string;
+  starts_at: string;
+  attendees_count: string;
+}
+
+const Event: React.FC<EventProps> = (props: EventProps) => {
+  return (
+    <View>
+      <Text>{props.name}</Text>
+      <Text>{props.description}</Text>
+      <Text>{props.starts_at}</Text>
+      <Text>{props.attendees_count}</Text>
+    </View>
+  );
+};
+
+export default Event;
